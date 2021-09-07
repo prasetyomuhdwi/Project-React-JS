31e2239dfed15634f66b619aaaaa411b3a8f9e6a
prasetyomuhdwi <lenovpras110@gmail.com>
2021-09-07 07:02:53 +0700

    [Edit] Index and [Add] shared-kernel.d

--- podomoro/src/index.js
+++ podomoro/src/index.js
@@ -1,29 +1,32 @@
 // Library //
 import reactDom from "react-dom";
-// import Card from "./Components/ContactCard"
-import NavBar from "./Components/NavBar"
-import Podomoro from "./Components/Podomoro";
-import ToDoList from "./Components/ToDoList";
-import './css/style.css';
+import Header from "./ui/Header/Header";
+import Podomoro from "./ui/Podomoro/Podomoro";
+import ToDoList from "./ui/Todolist/ToDoList";
+import styles from "./index.css";
 
 // init //
-const contact = {imgUrl:"logo512.png", name:"Title", phoneNum:"09273746573", email:"asdhg@adsadsa.com"}
+const init = {
+  imgUrl: "logo512.png",
+  name: "Title",
+  phoneNum: "09273746573",
+  email: "asdhg@adsadsa.com",
+};
 
 // Components //
 const App = () => {
-    return (
-    <div style={{display:"flex",justifyContent:"center", alignItems:"top",width:"100%",height:"100vh",backgroundColor: "tomato"}}>
-        <div style={{display:"flex", alignItems:"center",flexDirection:"column",width:"60%"}}>
-            <NavBar contact={contact}/>
-            <div style={{display:"flex"}}>
-                <Podomoro contact={contact} style={{width:"60%"}}/>
-                <ToDoList contact={contact} style={{width:"30%"}}/>
-            </div>
-            {/* <Card contact={contact}/> */}
+  return (
+    <div className="app">
+      <div className="content">
+        <Header contact={init} />
+        <div className="main">
+          <Podomoro contact={init} style={{ width: "60%" }} />
+          <ToDoList contact={init} style={{ width: "30%" }} />
         </div>
+      </div>
     </div>
-    )
-};  
+  );
+};
 
 // Render //
-reactDom.render(<App />,document.getElementById("root"))
+reactDom.render(<App />, document.getElementById("root"));
---
+++ podomoro/src/shared-kernel.d.ts
@@ -0,0 +1,5 @@
+// Type-alias to get rid of primitive obsession
+type Email = string;
+type UniqueId = string;
+type DateTimeString = string;
+type PriceCents = number;
